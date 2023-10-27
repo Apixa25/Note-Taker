346ad2f9a831ccd668900b3ad61e10ca61d568a3
stevensillsii <stevensills2@gmail.com>
2023-10-26 18:48:03 -0700

    working on server.js

--- server.js
+++ server.js
@@ -5,7 +5,10 @@ const api_routes = require('./Develop/routes/api-routes')
 const PORT = 3000;
 const app = express();
 
-// app.get('.', (req, res, next))
+app.get("/", (req, res) => {
+    console.log("Here")
+    res.render('index')
+})
 
 app.listen(PORT, () => {
     console.log(`This bad boy is running! at http://localhost:${PORT}`);
