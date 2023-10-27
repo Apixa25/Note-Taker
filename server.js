15fe628086e1ef2ad2793337a1508745875cd839
stevensillsii <stevensills2@gmail.com>
2023-10-26 20:24:43 -0700

    more server.js work

--- server.js
+++ server.js
@@ -8,12 +8,21 @@ const app = express();
 
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 
 app.use(express.static('public'));
 app.use(api_routes)
 app.use(html_routes)
 
+// GET Route for homepage
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/Develope/public/index.html'))
+);
+
+// GET Route for feedback page
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/Develope/public/notes.html'))
+);
 
 app.listen(PORT, () => {
     console.log(`This bad boy is running! at http://localhost:${PORT} 🚀`);
